b82cbc61bc8e3b47208728255b7dccf96efac965
Kailnestero <131260438+Kailnestero@users.noreply.github.com>
2023-05-05 10:40:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    document.getElementById("myDIV").style.display = "none";
+    document.getElementById("myDIV1").style.display = "block";
+  }
+
+
+  function myFunction1() {
+    document.getElementById("myDIV").style.display = "block";
+    document.getElementById("myDIV1").style.display = "none";
+  }
